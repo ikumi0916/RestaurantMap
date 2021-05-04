c650d5a13a9ef11ae926c19cf59bdfde58af2056
itoamacchi <t15e0333@gmail.com>
2021-05-04 13:23:23 +0900

    Mod Call GoogleMapAPI By Click

--- RestaurantMap/views.py
+++ RestaurantMap/views.py
@@ -3,20 +3,20 @@ from .models import Store
 from django.conf import settings
 
 """
-■IndexList
+■ListView
 「model」に設定したオブジェクトのhtmlを作成しておく必要がある
 【例】
 ・オブジェクト名：Store
 ・作成が必要なHTML：store_list.html
 """
-class IndexView(generic.ListView):
+class StoreView(generic.ListView):
     model = Store
     paginate_by = 10
 
     def get_context_data(self):
         ctx = super().get_context_data()
 
-        # page_title を追加する
+        # APIキーを取得する
         ctx['api_key'] = settings.GOOGLEMAP_API_KEY
 
         return ctx
